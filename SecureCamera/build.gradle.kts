4f42ef31574ac932912adefdb9575c5ab041ce73
toyota-m2k <toyota.mitsuki@gmail.com>
2024-07-09 20:54:14 +0900

    PrivCamera v1.7.5

--- SecureCamera/build.gradle.kts
+++ SecureCamera/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.7.4"
+        versionName = "1.7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
