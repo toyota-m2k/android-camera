99e9da1a2cbbdc90034be391a1de9c45149a3270
toyota-m2k <toyota.mitsuki@gmail.com>
2024-08-15 15:53:07 +0900

    targetSdk 35 / kotlin 2.0.10

--- monitor/build.gradle.kts
+++ monitor/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "io.github.toyota32k.monitor"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
