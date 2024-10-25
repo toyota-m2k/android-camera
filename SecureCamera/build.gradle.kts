932f4b4426a79ee18583d1a4a453f20ec1af5bcd
toyota-m2k <toyota.mitsuki@gmail.com>
2024-10-25 22:09:26 +0900

    libs: media-processor 2.15.1

--- SecureCamera/build.gradle.kts
+++ SecureCamera/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.16.5"
+        versionName = "1.16.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
