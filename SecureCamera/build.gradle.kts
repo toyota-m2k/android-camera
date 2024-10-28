2ec4d7459c652a79f790f699742b07b343d1525c
toyota-m2k <toyota.mitsuki@gmail.com>
2024-10-28 19:37:00 +0900

    AMP: 2.15.3

--- SecureCamera/build.gradle.kts
+++ SecureCamera/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.16.6"
+        versionName = "1.17.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
