56783e45fcd860624af476eefc98ba768f107c40
toyota-m2k <toyota.mitsuki@gmail.com>
2024-10-31 22:17:08 +0900

    libs: update dependencies : v1.17.5

--- SecureCamera/build.gradle.kts
+++ SecureCamera/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.17.4"
+        versionName = "1.17.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
