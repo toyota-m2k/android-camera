f07d5c652c32290c2cb4160d391a23a64ed7f033
toyota-m2k <toyota.mitsuki@gmail.com>
2025-04-12 12:34:01 +0900

    SA:v1.20.1 update libs

--- SecureCamera/build.gradle.kts
+++ SecureCamera/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.20.0"
+        versionName = "1.20.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
