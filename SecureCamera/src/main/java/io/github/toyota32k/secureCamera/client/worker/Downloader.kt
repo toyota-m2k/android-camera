73bb845661d58ca3b89dd1c4faa1b1586c28d2e0
toyota-m2k <toyota.mitsuki@gmail.com>
2025-07-22 11:03:26 +0900

    utils 4.5.1 (binding 1.9.7)

--- SecureCamera/src/main/java/io/github/toyota32k/secureCamera/client/worker/Downloader.kt
+++ SecureCamera/src/main/java/io/github/toyota32k/secureCamera/client/worker/Downloader.kt
@@ -1,90 +1,103 @@
 package io.github.toyota32k.secureCamera.client.worker
 
 import android.content.Context
+import androidx.work.Data
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import io.github.toyota32k.logger.UtLog
 import io.github.toyota32k.secureCamera.client.Canceller
 import io.github.toyota32k.secureCamera.client.NetClient
-import io.github.toyota32k.utils.android.IAwaiter
+import io.github.toyota32k.utils.android.IWorkerAwaiter
 import io.github.toyota32k.utils.android.ProgressWorker
-import io.github.toyota32k.utils.android.ProgressWorkerProcessor
+import io.github.toyota32k.utils.android.ProgressWorkerGenerator
+import io.github.toyota32k.utils.android.WorkerParams
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.ensureActive
 import okhttp3.Request
 import java.io.File
 
-object Downloader : ProgressWorkerProcessor() {
-    const val KEY_ITEM_ID = "itemId"
-    const val KEY_URL = "url"
-    const val KEY_FILE_PATH = "path"
-    val logger = UtLog("Worker")
+class Downloader(context: Context, params: WorkerParameters) : ProgressWorker(context, params) {
+    companion object {
+        val logger = UtLog("Worker")
 
-    class DLWorker(context: Context, params: WorkerParameters) : ProgressWorker(context, params) {
-        private fun error(message:String):Result {
-            logger.error(message)
-            return Result.failure(workDataOf("error" to message))
+        fun download(context:Context, itemId:Int, url:String, path:String, progress:(current:Long, total:Long)->Unit):IWorkerAwaiter {
+            logger.info("download: $itemId, $url, $path")
+            return ProgressWorkerGenerator
+                .builder<Downloader>()
+                .setInputData(DLTargetParams.produce(itemId, url, path))
+                .onProgress(progress)
+                .build(context)
         }
+    }
 
-        override suspend fun doWork(): Result {
-            val canceller = Canceller()
-            try {
-                val url = inputData.getString(KEY_URL) ?: return error("no url")
-                val file = File(inputData.getString(KEY_FILE_PATH) ?: return error("no file path"))
-                val request = Request.Builder()
-                    .url(url)
-                    .build()
-                return NetClient.executeAsync(request, canceller).use { response ->
-                        if (response.isSuccessful) {
-                            response.body?.use { body ->
-                                val totalLength =
-                                    response.headers["Content-Length"]?.toLongOrNull() ?: -1L
-                                body.byteStream().use { inStream ->
-                                    file.outputStream().use { outStream ->
-                                        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
-                                        var bytesCopied: Long = 0
-                                        var bytes = inStream.read(buffer)
-
-                                        while (bytes >= 0) {
-                                            currentCoroutineContext().ensureActive()
-                                            outStream.write(buffer, 0, bytes)
-                                            bytesCopied += bytes
-
-                                            // 進捗をSetProgressに設定
-                                            progress(bytesCopied, totalLength)
-                                            bytes = inStream.read(buffer)
-                                        }
-                                        outStream.flush()
-                                    }
-                                }
-                            }
-                            logger.info("completed")
-                            Result.success()
-                        } else {
-                            logger.error(response.message)
-                            error(response.message)
-                        }
-                    }
-            } catch (_: CancellationException) {
-                logger.info("cancelled")
-                canceller.cancel()
-                return error("cancelled")
-            } catch (e: Throwable) {
-                logger.error(e)
-                canceller.cancel()
-                return error(e.message ?: "unknown error")
+    private class DLTargetParams(inputData: Data?) : WorkerParams(inputData) {
+        var itemId:Int by delegate.intMinusOne
+        var url:String? by delegate.stringNullable
+        var filePath:String? by delegate.stringNullable
+        companion object {
+            fun produce(itemId:Int, url:String, filePath:String):Data {
+                return DLTargetParams(null).apply {
+                    this.itemId = itemId
+                    this.url = url
+                    this.filePath = filePath
+                }.produce()
             }
         }
     }
 
-    fun download(context:Context, itemId:Int, url:String, path:String, progress:((current:Long, total:Long)->Unit)?):IAwaiter<Boolean> {
-        logger.info("download: $itemId, $url, $path")
-        return process<DLWorker>(context,
-            workDataOf(
-                KEY_URL to url,
-                KEY_FILE_PATH to path,
-                KEY_ITEM_ID to itemId,
-            ), progress)
+    private fun error(message:String):Result {
+        logger.error(message)
+        return Result.failure(workDataOf("error" to message))
+    }
+
+    override suspend fun doWork(): Result {
+        val canceller = Canceller()
+        try {
+            var target = DLTargetParams(inputData)
+            val url = target.url ?: return error("no url")
+            val file = File(target.filePath ?: return error("no file path"))
+            val request = Request.Builder()
+                .url(url)
+                .build()
+            return NetClient.executeAsync(request, canceller).use { response ->
+                    if (response.isSuccessful) {
+                        response.body?.use { body ->
+                            val totalLength = response.headers["Content-Length"]?.toLongOrNull() ?: -1L
+                            body.byteStream().use { inStream ->
+                                file.outputStream().use { outStream ->
+                                    val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+                                    var bytesCopied: Long = 0
+                                    var bytes = inStream.read(buffer)
+
+                                    while (bytes >= 0) {
+                                        currentCoroutineContext().ensureActive()
+                                        outStream.write(buffer, 0, bytes)
+                                        bytesCopied += bytes
+
+                                        // 進捗をSetProgressに設定
+                                        progress(bytesCopied, totalLength)
+                                        bytes = inStream.read(buffer)
+                                    }
+                                    outStream.flush()
+                                }
+                            }
+                        }
+                        logger.info("completed")
+                        Result.success()
+                    } else {
+                        logger.error(response.message)
+                        error(response.message)
+                    }
+                }
+        } catch (_: CancellationException) {
+            logger.info("cancelled")
+            canceller.cancel()
+            return error("cancelled")
+        } catch (e: Throwable) {
+            logger.error(e)
+            canceller.cancel()
+            return error(e.message ?: "unknown error")
+        }
     }
 }
--- SecureCamera/src/main/java/io/github/toyota32k/secureCamera/client/worker/Uploader.kt
+++ SecureCamera/src/main/java/io/github/toyota32k/secureCamera/client/worker/Uploader.kt
@@ -3,7 +3,6 @@ package io.github.toyota32k.secureCamera.client.worker
 import android.content.Context
 import androidx.work.Data
 import androidx.work.WorkerParameters
-import androidx.work.workDataOf
 import io.github.toyota32k.secureCamera.client.Canceller
 import io.github.toyota32k.secureCamera.client.NetClient.executeAsync
 import io.github.toyota32k.secureCamera.client.ProgressRequestBody
@@ -13,9 +12,10 @@ import io.github.toyota32k.secureCamera.db.ItemEx
 import io.github.toyota32k.secureCamera.db.MetaDB
 import io.github.toyota32k.secureCamera.db.ScDB
 import io.github.toyota32k.secureCamera.settings.Settings
-import io.github.toyota32k.utils.android.IAwaiter
+import io.github.toyota32k.utils.android.IWorkerAwaiter
 import io.github.toyota32k.utils.android.ProgressWorker
-import io.github.toyota32k.utils.android.ProgressWorkerProcessor
+import io.github.toyota32k.utils.android.ProgressWorkerGenerator
+import io.github.toyota32k.utils.android.WorkerParams
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.isActive
@@ -26,117 +26,121 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 
-object Uploader : ProgressWorkerProcessor() {
-    const val KEY_CONTENT_TYPE = "contentType"
-    const val KEY_OWNER_ID = "ownerId"
-    const val KEY_SLOT = "slot"
-    const val KEY_FILE_DATE = "fileDate"
-    const val KEY_CREATION_DATE = "creationDate"
-    const val KEY_ORIGINAL_ID = "originalId"
-    const val KEY_META_INFO = "metaInfo"
-    const val KEY_EXT_ATTR = "extAttr"
-    const val KEY_ITEM_NAME = "name"
-    const val KEY_FILE_PATH = "path"
-    const val KEY_DURATION = "duration"
+class Uploader(context: Context, params: WorkerParameters) : ProgressWorker(context, params) {
+    companion object {
+        val logger get() = Downloader.logger
 
-    fun workDataFromItem(db:ScDB, item: ItemEx):Data {
-        return workDataOf(
-            KEY_CONTENT_TYPE to if(item.isPhoto) "image/png" else "video/mp4",
-            KEY_OWNER_ID to Settings.SecureArchive.clientId,
-            KEY_SLOT to item.slot,
-            KEY_FILE_DATE to "${item.date}",
-            KEY_CREATION_DATE to "${item.creationDate}",
-            KEY_ORIGINAL_ID to "${item.id}",
-            KEY_META_INFO to "",
-            KEY_EXT_ATTR to "${item.attrDataJson}",
-            KEY_ITEM_NAME to item.name,
-            KEY_FILE_PATH to db.fileOf(item).absolutePath,
-            KEY_DURATION to "${item.duration}",
-        )
+        fun upload(context:Context, db:ScDB, item: ItemEx, progress:(current:Long, total:Long)->Unit):IWorkerAwaiter {
+            logger.info("upload: ${item.id}, ${item.name}")
+            return ProgressWorkerGenerator
+                .builder<Uploader>()
+                .setInputData(ULTargetParams.produce(db, item))
+                .onProgress(progress)
+                .build(context)
+        }
     }
+    private class ULTargetParams(inputData: Data?=null) : WorkerParams(inputData) {
+        var contentType:String by delegate.string
+        var ownerId:String by delegate.string
+        var slot:Int by delegate.intZero
+        var fileDate:Long by delegate.longZero
+        var creationDate:Long by delegate.longZero
+        var originalId:Int by delegate.intZero
+        var metaInfo:String by delegate.string
+        var extAttr:String by delegate.string
+        var itemName:String by delegate.string
+        var filePath:String by delegate.string
+        var duration:Long by delegate.longZero
 
-    class ULWorker(context: Context, params: WorkerParameters) : ProgressWorker(context, params) {
-        override suspend fun doWork(): Result {
-            val filePath = inputData.getString(KEY_FILE_PATH) ?: return Result.failure()
-            val file = File(filePath)
-            if (!file.exists()) {
-                logger.error("file not found: $filePath")
-                return Result.failure()
+        constructor(db:ScDB, item: ItemEx):this() {
+            contentType = if(item.isPhoto) "image/png" else "video/mp4"
+            ownerId = Settings.SecureArchive.clientId
+            slot = item.slot
+            fileDate = item.date
+            creationDate = item.creationDate
+            originalId = item.id
+            metaInfo = ""
+            extAttr = "${item.attrDataJson}"
+            itemName = item.name
+            filePath = db.fileOf(item).absolutePath
+            duration = item.duration
+        }
+        companion object {
+            fun produce(db:ScDB, item: ItemEx):Data {
+                return ULTargetParams(db, item).produce()
             }
-            val contentType = inputData.getString(KEY_CONTENT_TYPE) ?: return Result.failure()
-            val ownerId = inputData.getString(KEY_OWNER_ID) ?: return Result.failure()
-            val slot = inputData.getInt(KEY_SLOT, 0)
-            val fileDate = inputData.getString(KEY_FILE_DATE) ?: return Result.failure()
-            val creationDate = inputData.getString(KEY_CREATION_DATE) ?: return Result.failure()
-            val originalId = inputData.getString(KEY_ORIGINAL_ID) ?: return Result.failure()
-            val metaInfo = inputData.getString(KEY_META_INFO) ?: ""
-            val extAttr = inputData.getString(KEY_EXT_ATTR) ?: ""
-            val itemName = inputData.getString(KEY_ITEM_NAME) ?: ""
-            val duration = inputData.getString(KEY_DURATION) ?: ""
-            val canceller = Canceller()
-            val workerContext = currentCoroutineContext()
-            var cancelled = false
-            return try {
-                val body =
-                    ProgressRequestBody(file.asRequestBody(contentType.toMediaType())) { current, total ->
-                        runBlocking {
-                            // ここで例外をスローしたら Call がいい感じに例外を投げてキャンセルされるかと期待したが、
-                            // なにやら大切なスレッドが死ぬためかアプリが落ちてしまう。
-                            // workerContext.ensureActive()
-                            if(!workerContext.isActive) {
-                                logger.info("cancelling")
-                                cancelled = true
-                                canceller.cancel()
-                                return@runBlocking
-                            }
-                            progress(current, total)
-                            logger.info("progress: $current / $total")
-                        }
-                    }
-                val multipartBody = MultipartBody.Builder()
-                    .setType(MultipartBody.FORM)
-                    .addFormDataPart("OwnerId", ownerId)
-                    .addFormDataPart("Slot", slot.toString())
-                    .addFormDataPart("FileDate", fileDate)
-                    .addFormDataPart("CreationDate", creationDate)
-                    .addFormDataPart("OriginalId", originalId)
-                    .addFormDataPart("MetaInfo", metaInfo)
-                    .addFormDataPart("ExtAttr", extAttr)
-                    .addFormDataPart("File", itemName, body)
-                    .addFormDataPart("Duration", duration)
-                    .build()
-                val request = Request.Builder()
-                    .url("http://${Authentication.activeHostAddress}/slot$slot/upload")
-                    .post(multipartBody)
-                    .build()
-                val code = executeAsync(request, canceller).use {
-                    it.code
-                }
-                if (code == 200) {
-                    logger.debug("uploaded")
-                    MetaDB.withDB { db-> db.updateCloud(originalId.toInt(), CloudStatus.Uploaded)}
-                    Result.success()
-                } else {
-                    logger.error("unexpected status code: $code")
-                    Result.failure()
-                }
-            } catch (_: CancellationException) {
-                logger.info("cancelled")
-                canceller.cancel()
-                Result.failure()
-            } catch (e: Throwable) {
-                if(cancelled) {
-                    logger.info("cancelled: ${e.message}")
-                } else {
-                    logger.error(e)
-                }
-                canceller.cancel()
-                Result.failure()
+            fun consume(inputData: Data):ULTargetParams {
+                return ULTargetParams(inputData)
             }
         }
     }
 
-    fun upload(context: Context, db:ScDB, item:ItemEx, progress:((current:Long, total:Long)->Unit)?):IAwaiter<Boolean> {
-        return process<ULWorker>(context, workDataFromItem(db, item), progress)
+    override suspend fun doWork(): Result {
+        val target = ULTargetParams.consume(inputData)
+        val file = File(target.filePath)
+        if (!file.exists()) {
+            logger.error("file not found: ${target.filePath}")
+            return Result.failure()
+        }
+
+        val canceller = Canceller()
+        val workerContext = currentCoroutineContext()
+        var cancelled = false
+        return try {
+            val body = ProgressRequestBody(file.asRequestBody(target.contentType.toMediaType())) { current, total ->
+                    runBlocking {
+                        // ここで例外をスローしたら Call がいい感じに例外を投げてキャンセルされるかと期待したが、
+                        // なにやら大切なスレッドが死ぬためかアプリが落ちてしまう。
+                        // workerContext.ensureActive()
+                        if (!workerContext.isActive) {
+                            logger.info("cancelling")
+                            cancelled = true
+                            canceller.cancel()
+                            return@runBlocking
+                        }
+                        progress(current, total)
+                        logger.info("progress: $current / $total")
+                    }
+                }
+            val multipartBody = MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("OwnerId", target.ownerId)
+                .addFormDataPart("Slot", target.slot.toString())
+                .addFormDataPart("FileDate", "${target.fileDate}")
+                .addFormDataPart("CreationDate", "${target.creationDate}")
+                .addFormDataPart("OriginalId", "${target.originalId}")
+                .addFormDataPart("MetaInfo", target.metaInfo)
+                .addFormDataPart("ExtAttr", target.extAttr)
+                .addFormDataPart("Duration", "${target.duration}")
+                .addFormDataPart("File", target.itemName, body)
+                .build()
+            val request = Request.Builder()
+                .url("http://${Authentication.activeHostAddress}/slot${target.slot}/upload")
+                .post(multipartBody)
+                .build()
+            val code = executeAsync(request, canceller).use {
+                it.code
+            }
+            if (code == 200) {
+                logger.debug("uploaded")
+                MetaDB.withDB { db -> db.updateCloud(target.originalId, CloudStatus.Uploaded) }
+                Result.success()
+            } else {
+                logger.error("unexpected status code: $code")
+                Result.failure()
+            }
+        } catch (_: CancellationException) {
+            logger.info("cancelled")
+            canceller.cancel()
+            Result.failure()
+        } catch (e: Throwable) {
+            if (cancelled) {
+                logger.info("cancelled: ${e.message}")
+            } else {
+                logger.error(e)
+            }
+            canceller.cancel()
+            Result.failure()
+        }
     }
 }
