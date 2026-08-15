521bc33bff40e7b882dee544238daf3622c981ae
toyota-m2k <toyota.mitsuki@gmail.com>
2026-08-15 12:44:11 +0900

    libs: player 4.10.2 / fix slide show bug

--- SecureCamera/build.gradle.kts
+++ SecureCamera/build.gradle.kts
@@ -38,7 +38,7 @@ configure<ApplicationExtension> {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "2.36.0"
+        versionName = "2.36.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
